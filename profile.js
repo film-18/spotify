1d74868408980fa8b1072ae2a750c8997aecddae
film <film_kanlaya@hotmail.com>
2020-11-02 18:16:43 +0700

    login

--- profile.js
+++ profile.js
@@ -3,6 +3,9 @@ firebase.initializeApp({
     projectId: 'spotify-8da9e'
 });
 
+// window.onload = () => {
+// }
+
 let mail = ''
 let currentChar = ''
 let index = document.cookie.search("now=") + 4;
@@ -13,48 +16,86 @@ while (currentChar !== "-") {
 }
 
 let name = mail.slice(0, -1);
-let addSong = [];
-let addArtist = [];
-let addimg = [];
+// let addSong = [];
+// let addArtist = [];
+// let addimg = [];
 
-function likeSong() {
+async function likeSong() {
+    let mail = ''
+    let currentChar = ''
+    let index = document.cookie.search("now=") + 4;
+    while (currentChar !== "-") {
+        currentChar = document.cookie[index];
+        mail += currentChar;
+        index += 1;
+    }
+
+    let name = mail.slice(0, -1);
+
     let nameSong = document.getElementById("songti").innerHTML;
     let nameArtist = document.getElementById("songar").innerHTML;
     let img = document.getElementById("thumbnail").getAttribute("src");
-    addSong.push(nameSong);
-    addArtist.push(nameArtist);
-    addimg.push(img);
+    // addSong.push(nameSong);
+    // addArtist.push(nameArtist);
+    // addimg.push(img);
     // addSong.push(addArtist);
 
+    let info = {
+        artist:[],
+        nameSong:[],
+        img:[],
+    }
     var db = firebase.firestore();
-    db.collection("likeSong").doc(name).set({
-        Name: name,
-        Song: {
-            Artist: addArtist,
-            NameSong: addSong,
-            Img: addimg
-        }
+    let doc = await db.collection("likeSong").doc(name).get()
+    info = {
+        Artist: doc.data().Song.Artist,
+        NameSong: doc.data().Song.NameSong,
+        Img: doc.data().Song.Img
+    };
+    console.log(info)
+    //console.log(doc.data())
+    //console.log(doc.data())
+    let data = doc.data();
+    info.Artist.push(nameArtist)
+    info.NameSong.push(nameSong)
+    info.Img.push(img)
+    // data.Song.Artist.push(nameArtist)
+    // data.Song.NameSong.push(nameSong)
+    // data.Song.Img.push(img)
+    console.log(info)
+    await db.collection("likeSong").doc(name).update({
+        // Song: {
+        //     Artist: firebase.firestore.FieldValue.arrayUnion(nameArtist),
+        //     NameSong: firebase.firestore.FieldValue.arrayUnion(nameSong),
+        //     Img: firebase.firestore.FieldValue.arrayUnion(img)
 
+        // }
+        Song: {
+            Artist: info.Artist,
+            NameSong: info.NameSong,
+            Img: info.Img
+        }
 
     })
 
     console.log(nameSong);
-    console.log(addSong);
-    console.log(addArtist);
+    console.log(nameArtist);
+    console.log(img);
 }
 
+//show playlist
 var db = firebase.firestore();
 db.collection("playlist").doc(name).get().then(data => {
 
-    for (let i = 0; i < data.data().Playlist.play.length; i++) {
-        let table = document.getElementById("table0");
-        table.setAttribute("class", "border-top-0 pb-3"); //id @table
-        let row = table.insertRow(-1);
-        let newrow = row.insertCell(0);
-        let playlist1 = data.data().Playlist.play[i];
-        newrow.innerHTML = playlist1;
+    for (let i = 0; i < data.data().Playlist.length; i++) {
+    let table = document.getElementById("table0"); //id @table
+    table.setAttribute("class", "border-top-0 pb-3");
+    let row = table.insertRow(-1);
+    let newrow = row.insertCell(0);
+    let playlist1 = data.data().Playlist[i];
+    newrow.innerHTML = playlist1;
 
-        newrow.style.color = "white";
+    newrow.style.color = "white";
     }
     console.log(data.data().Playlist);
 });
--- script.js
+++ script.js
@@ -1,40 +1,30 @@
-// Initialize Cloud Firestore through Firebase
-firebase.initializeApp({
-    apiKey: 'AIzaSyAIlXUZEeQ9jwTaIGr5C9uEUQYOd8KEWpA',
-    projectId: 'spotify-8da9e'
-});
-
-var db = firebase.firestore();
-function register() {
-
-
-
+async function register() {
+    
+    // Initialize Cloud Firestore through Firebase
+    firebase.initializeApp({
+        apiKey: 'AIzaSyAIlXUZEeQ9jwTaIGr5C9uEUQYOd8KEWpA',
+        projectId: 'spotify-8da9e'
+    });
+    
+    
     let email = document.getElementById('email').value;
     // let confirmEmail = document.getElementById('confirmemail').value;
-    let password = document.getElementById('password').value;
-    let namehelp = document.getElementById('name').value;
-    let day = document.getElementById('date').value;
-    let month = document.getElementById('month').value;
-    let year = document.getElementById('numyear').value;
-    let birth = day + "/" + month + "/" + year;
-    let gender = document.getElementById('choose');
-
+    // let password = document.getElementById('password').value;
+    // let namehelp = document.getElementById('name').value;
+    // let day = document.getElementById('date').value;
+    // let month = document.getElementById('month').value;
+    // let year = document.getElementById('numyear').value;
+    // let birth = day + "/" + month + "/" + year;
+    // let gender = document.getElementById('choose');
+    
     console.log(email);
-    console.log(gender);
-
-    db.collection("users").doc(email).set({
-        Email: email,
+    
+    var db = firebase.firestore();
+    await db.collection("playlist").doc('aaaa').set({
+        Email: 'aaaa'
         // ConfirmEmail: confirmEmail
-        Password: password,
-        Name: namehelp,
-        Birthday: birth,
-        Gender: gender
     })
 
-
-    // return aString;)
-
-    // location.href = "login.html";
 
     if (document.account.email.value == "") {
         alert("Enter some value!");
@@ -42,12 +32,5 @@ function register() {
     }
     document.cookie = "now=" + escape(document.account.email.value) + "-";
     // document.cookie = cookievalue;
-
-
-
-
-
-
-
 
 }
