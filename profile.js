b46a1f0cadc6e3b24bfce274b3e4d5d8b2301cc6
film <film_kanlaya@hotmail.com>
2020-11-03 10:23:37 +0700

    likesong

--- profile.js
+++ profile.js
@@ -1,10 +1,11 @@
+// ---------- Init Firebase ----------
+
 firebase.initializeApp({
     apiKey: 'AIzaSyAIlXUZEeQ9jwTaIGr5C9uEUQYOd8KEWpA',
     projectId: 'spotify-8da9e'
 });
 
-// window.onload = () => {
-// }
+// ---------- Get Mail ----------
 
 let mail = ''
 let currentChar = ''
@@ -16,11 +17,10 @@ while (currentChar !== "-") {
 }
 
 let name = mail.slice(0, -1);
-// let addSong = [];
-// let addArtist = [];
-// let addimg = [];
+
 
 async function likeSong() {
+    const db = firebase.firestore();
     let mail = ''
     let currentChar = ''
     let index = document.cookie.search("now=") + 4;
@@ -40,62 +40,67 @@ async function likeSong() {
     // addimg.push(img);
     // addSong.push(addArtist);
 
-    let info = {
-        artist:[],
-        nameSong:[],
-        img:[],
-    }
-    var db = firebase.firestore();
-    let doc = await db.collection("likeSong").doc(name).get()
-    info = {
-        Artist: doc.data().Song.Artist,
-        NameSong: doc.data().Song.NameSong,
-        Img: doc.data().Song.Img
-    };
-    console.log(info)
-    //console.log(doc.data())
-    //console.log(doc.data())
+    // let info = {
+    //     Artist:[],
+    //     NameSong:[],
+    //     Img:[],
+    // }
+    // let doc = await db.collection("likeSong").doc(name).get().then(data => {
+    //     console.log(data.data().name)
+    //     info = {
+    //         Artist: data.data().Song.Artist ? data.data().Song.Artist : [],
+    //         NameSong: data.data().Song.NameSong ? data.data().Song.NameSong : [],
+    //         Img: data.data().Song.Img ? data.data().Song.Img : []
+    //     };
+    // })
+    // console.log(info)
+
+    // let data = doc.data();
+    // info.Artist.push(nameArtist)
+    // info.NameSong.push(nameSong)
+    // info.Img.push(img)
+    let doc = await db.collection('likeSong').doc(name).get();
     let data = doc.data();
-    info.Artist.push(nameArtist)
-    info.NameSong.push(nameSong)
-    info.Img.push(img)
-    // data.Song.Artist.push(nameArtist)
-    // data.Song.NameSong.push(nameSong)
-    // data.Song.Img.push(img)
-    console.log(info)
-    await db.collection("likeSong").doc(name).update({
+
+    data.Song.Artist.push(nameArtist)
+    data.Song.NameSong.push(nameSong)
+    data.Song.Img.push(img)
+    await db.collection("likeSong").doc(name).set({
         // Song: {
         //     Artist: firebase.firestore.FieldValue.arrayUnion(nameArtist),
         //     NameSong: firebase.firestore.FieldValue.arrayUnion(nameSong),
         //     Img: firebase.firestore.FieldValue.arrayUnion(img)
-
         // }
         Song: {
-            Artist: info.Artist,
-            NameSong: info.NameSong,
-            Img: info.Img
+            Artist: data.Song.Artist,
+            NameSong: data.Song.NameSong,
+            Img: data.Song.Img
         }
-
-    })
+    }, {merge: true})
 
     console.log(nameSong);
     console.log(nameArtist);
     console.log(img);
 }
 
-//show playlist
+//show playlist 
 var db = firebase.firestore();
 db.collection("playlist").doc(name).get().then(data => {
 
     for (let i = 0; i < data.data().Playlist.length; i++) {
-    let table = document.getElementById("table0"); //id @table
-    table.setAttribute("class", "border-top-0 pb-3");
-    let row = table.insertRow(-1);
-    let newrow = row.insertCell(0);
-    let playlist1 = data.data().Playlist[i];
-    newrow.innerHTML = playlist1;
+        let table = document.getElementById("table0"); //id @table
+        table.setAttribute("class", "border-top-0 pb-3");
+        let row = table.insertRow(-1);
+        let newrow = row.insertCell(0);
+        let playlist1 = data.data().Playlist[i];
+        newrow.innerHTML = playlist1;
 
-    newrow.style.color = "white";
+        newrow.style.color = "white";
     }
     console.log(data.data().Playlist);
 });
+
+
+
+
+
