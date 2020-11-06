6cfd27243613ca38c9fbbd9d8643401ca0c527fe
film <film_kanlaya@hotmail.com>
2020-11-06 12:24:26 +0700

    eiei

---
+++ scirpt/follow.js
@@ -0,0 +1,26 @@
+// firebase.initializeApp({
+//     apiKey: 'AIzaSyAIlXUZEeQ9jwTaIGr5C9uEUQYOd8KEWpA',
+//     projectId: 'spotify-8da9e'
+// });
+
+// const db = firebase.firestore();
+
+async function follow() {
+    let personFollow = document.getElementById("personFollow").innerHTML;
+
+    let mail2 = ''
+    let currentChar2 = ''
+    let index2 = document.cookie.search("now=") + 4;
+    while (currentChar2 !== "-") {
+        currentChar2 = document.cookie[index2];
+        mail2 += currentChar2;
+        index2 += 1;
+    }
+
+    let name2 = mail2.slice(0, -1);
+    await db.collection("follow").doc(name2).set({
+        following: [personFollow]
+    }, {merge: true})
+    console.log(1)
+
+}
